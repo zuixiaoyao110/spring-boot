266d17e5b766167db1034582789a71ba9495910f
zuixiaoyao110 <test@runoob.com>
2018-11-30 14:40:10 +0800

    备忘

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    //https://github.com/zuixiaoyao110/spring-boot.git
+	//项目地址
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
